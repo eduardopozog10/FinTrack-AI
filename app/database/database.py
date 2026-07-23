c83e91d0fdc3a2ae488c4dc373ab4f3c5b32c978
eduardopozog10 <eduardopozog6@gmail.com>
2026-07-23 09:04:52 -0400

    Integracion database

---
+++ app/database/database.py
@@ -0,0 +1,18 @@
+from sqlmodel import SQLModel, Session, create_engine
+from app.models.expense import Expense
+
+DATABASE_URL = "sqlite:///fintrack.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.api.routes import router
 from app.core.config import settings
+from app.database.database import create_db_and_tables
 
 
 app = FastAPI(
@@ -9,5 +10,11 @@ app = FastAPI(
     description=settings.description,
     version=settings.version,
 )
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
 
 app.include_router(router)
